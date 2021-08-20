c049582cfde9987d331f1abe448e8fdacf55e703
Salim66 <salimhasanriad8@gmail.com>
2021-08-20 15:59:09 +0600

    Inserting DAta

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Product;
+use App\Models\Staff;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+/*
+|--------------------------------------------------------------------------
+| POLYMORPHIC RELATIONSHIP
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/create', function(){
+
+    // $staff = Staff::findOrFail(1);
+    $product = Product::findOrFail(1);
+    $product->photo()->create(['path' => 'example_2.jpg']);
+
+});
+
