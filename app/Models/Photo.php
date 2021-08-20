1bdc4e46f9aad872db484f02288fbea6cb44a3d3
Salim66 <salimhasanriad8@gmail.com>
2021-08-20 15:45:00 +0600

    Relationship and Mass Assignment

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Photo extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function imageable(){
+        return $this->morphTo();
+    }
+
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function photo(){
+        return $this->morphMany('App\Models\Photo', 'imageable');
+    }
+
 }
